e88b0249e6112a8e47bb2e600c8c0c3e6430127e
Edavilanava <112099434+Edavilanava@users.noreply.github.com>
2022-09-16 21:02:52 -0400

    turn in

---
+++ Easy/easy.js
@@ -0,0 +1,8 @@
+let str1 = "Danny"
+let str2 = "Bob"
+
+let result1 = str1.length;
+let result2 = str2.length;
+let final = result1-result2;
+
+console.log(`The name ${str2} is shorter than ${str1} by ${final}`);
---
+++ Very-Easy/app.js
@@ -0,0 +1,5 @@
+var num1 = 6
+var num2 = 2
+
+console.log(num1 - num2)
+console.log('The difference between 6 and 2 is 4')
